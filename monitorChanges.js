1b567de6de30d5e9285b20bee97388a796c364d4
Cris Stringfellow <cris7fe@icloud.com>
2019-01-24 15:40:57 +0000

    Add new file

---
+++ monitorChanges.js
@@ -0,0 +1,15 @@
+  // demo of watching for any new nodes that declare stylists
+  const mo = new MutationObserver((mutations)=> {
+    const AddedElements = [];
+    const RemovedElements = [];
+    for ( const mutation of mutations ) {
+      const addedElements = Array.from(mutation.addedNodes).filter(x => x.nodeType == Node.ELEMENT_NODE && x.hasAttribute('stylist'));
+      const removedElements = Array.from(mutation.removedNodes).filter(x => x.nodeType == Node.ELEMENT_NODE && x.hasAttribute('stylist'));
+      AddedElements.push(...addedElements);
+      RemovedElements.push(...removedElements);
+    }
+    if ( AddedElements.length || RemovedElements.length ) {
+      console.log({AddedElements,RemovedElements});
+    }
+  });
+  mo.observe(document.documentElement, {childList:true, subtree:true});
