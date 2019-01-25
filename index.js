0e786ec3bc17c307a92b5637b7e4570b2dc2310b
Cris <22254235+crislin2046@users.noreply.github.com>
2019-01-25 14:00:24 +0000

    Basic functionality works. YAY.

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 import {dss} from './dss.js';
+import {generateUniquePrefix, prefixAllRules} from './c3s/c3s.js';
 import {addInsertListener, addRemovedListener, monitorChanges} from './monitorChanges.js';
 
 const stylistFunctions = new Map();
 
+function associate(className, element, stylist, state) {
+  const styleText = stylist(element, state) || '';
+  const styleMarkup = `
+    <style data-prefix="${className}">
+      ${styleText}
+    </style>
+  `;
+  document.head.insertAdjacentHTML('beforeEnd', styleMarkup);
+  const styleElement = document.head.querySelector(`style[data-prefix="${className}"]`);
+  const styleSheet = styleElement.sheet;
+  prefixAllRules(styleSheet, "." + className);
+  element.setAttribute('associated', 'true');
+}
+
+function disassociate(className, element) {
+  const styleSheet = document.querySelector(`style[data-prefix="${className}"]`); 
+  if ( !! styleSheet ) {
+    element.classList.remove(className);
+    styleSheet.remove();
+  }
+}
+
 export function initializeDSS(functionsObject) {
+  document.head.insertAdjacentHTML('afterBegin', `
+    <style data-role="prevent-fouc">
+      [stylist]:not([associated]) {
+        display: none !important;
+      }
+    </style>
+  `);
   addMoreStylistFunctions(functionsObject); 
   addInsertListener(associateStylistFunctions);
   addRemovedListener(unassociateStylistFunctions);
@@ -14,13 +44,21 @@ export function initializeDSS(functionsObject) {
   function associateStylistFunctions(...els) {
     els = els.filter(el => el.hasAttribute('stylist'));
     if ( els.length == 0 ) return;
-    console.log("Stylist els added", els);
+    for ( const el of els ) {
+      const stylistName = el.getAttribute('stylist');
+      const stylist = stylistFunctions.get(stylistName);
+      const className = randomClass();
+      el.classList.add(className);
+      associate(className, el, stylist);
+    }
   }
 
   function unassociateStylistFunctions(...els) {
     els = els.filter(el => el.hasAttribute('stylist'));
     if ( els.length == 0 ) return;
-    console.log("Stylist els removed", els);
+    for ( const el of els ) {
+      el.classList.forEach(className => className.startsWith('c3s') && disassociate(className, el));
+    }
   }
 }
 
@@ -35,3 +73,7 @@ export function addMoreStylistFunctions(functionsObject) {
   while(toRegister.length) toRegister.pop()();
 }
 
+function randomClass() {
+  const {prefix:className} = generateUniquePrefix();
+  return className;
+}
